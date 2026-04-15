7bb5b11228f062e9877f4176c267bccd94b178a2
Ronald Miles <ron.miles@1000paces.com>
2026-04-15 06:58:14 -0700

    Adds shell for system test for GH CI/CD

---
+++ test/system/initial_setups_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class InitialSetupsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit initial_setups_url
+  #
+  #   assert_selector "h1", text: "InitialSetup"
+  # end
+end
